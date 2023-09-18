0c34ca7f6ee3b49bd7448f979ca8adb2438dab72
Jason <123jasont@gmail.com>
2023-09-17 23:58:41 -0700

    fix: broken `BentoBox` css

--- components/BentoBox.tsx
+++ components/BentoBox.tsx
@@ -6,7 +6,7 @@ import { Responsive, WidthProvider } from 'react-grid-layout'
 const ResponsiveGridLayout = WidthProvider(Responsive)
 
 const BentoBox = () => {
-    const layout = [
+    const mainLayout = [
         { i: 'a', x: 0, y: 0, w: 2, h: 1 },
         { i: 'b', x: 2, y: 0, w: 1, h: 1 },
         { i: 'c', x: 4, y: 0, w: 1, h: 2 },
@@ -18,6 +18,20 @@ const BentoBox = () => {
         { i: 'i', x: 1, y: 4, w: 1, h: 1 },
         { i: 'j', x: 2, y: 4, w: 2, h: 1 },
     ]
+
+    const mobileLayout = [
+        { i: 'a', x: 0, y: 0, w: 2, h: 1 }, // A (2x1)
+        { i: 'e', x: 0, y: 1, w: 2, h: 1 }, // E (2x1)
+        { i: 'f', x: 0, y: 2, w: 1, h: 2 }, // F (1x2)
+        { i: 'b', x: 1, y: 2, w: 1, h: 1 }, // B (1x1)
+        { i: 'd', x: 0, y: 4, w: 1, h: 1 }, // D (1x1)
+        { i: 'c', x: 1, y: 3, w: 1, h: 2 }, // C (1x2)
+        { i: 'g', x: 0, y: 5, w: 2, h: 1 }, // G (2x1)
+        { i: 'h', x: 0, y: 6, w: 1, h: 1 }, // H (1x1)
+        { i: 'i', x: 1, y: 6, w: 1, h: 1 }, // I (1x1)
+        { i: 'j', x: 0, y: 7, w: 2, h: 1 }, // J (2x1)
+    ];
+    
 
     const [rowHeight, setRowHeight] = useState(280)
 
@@ -35,8 +49,8 @@ const BentoBox = () => {
         <div className="react-grid-container">
             <ResponsiveGridLayout
                 className="layout"
-                layouts={{ lg: layout, md: layout, sm: layout }}
-                breakpoints={{ lg: 1100, md: 799, sm: 374 }}
+                layouts={{ lg: mainLayout, md: mainLayout, sm: mobileLayout }}
+                breakpoints={{ lg: 1199, md: 799, sm: 374 }}
                 cols={{ lg: 4, md: 4, sm: 2 }}
                 rowHeight={rowHeight}
                 isResizable={false}
