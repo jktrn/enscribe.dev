12f0e4c44141d6c52e75a6d519447c07b5adc8d9
Jason <123jasont@gmail.com>
2023-09-19 00:16:20 -0700

    fix(bento): static mobile layout, fixed svgs

--- components/bento/BentoBox.tsx
+++ components/bento/BentoBox.tsx
@@ -27,17 +27,17 @@ const BentoBox = ({ posts }) => {
     ]
 
     const mobileLayout = [
-        { i: 'a', x: 0, y: 0, w: 2, h: 1 },
-        { i: 'e', x: 0, y: 1, w: 2, h: 1 },
-        { i: 'f', x: 0, y: 2, w: 1, h: 2 },
-        { i: 'b', x: 1, y: 2, w: 1, h: 1 },
-        { i: 'd', x: 0, y: 4, w: 1, h: 1 },
-        { i: 'c', x: 1, y: 3, w: 1, h: 2 },
-        { i: 'g', x: 0, y: 5, w: 1, h: 1 },
-        { i: 'h', x: 1, y: 5, w: 1, h: 1 },
-        { i: 'i', x: 0, y: 6, w: 1, h: 1 },
-        { i: 'j', x: 1, y: 6, w: 1, h: 1 },
-        { i: 'k', x: 0, y: 7, w: 2, h: 1 },
+        { i: 'a', x: 0, y: 0, w: 2, h: 1, static: true },
+        { i: 'e', x: 0, y: 1, w: 2, h: 1, static: true },
+        { i: 'f', x: 0, y: 2, w: 1, h: 2, static: true },
+        { i: 'b', x: 1, y: 2, w: 1, h: 1, static: true },
+        { i: 'd', x: 0, y: 4, w: 1, h: 1, static: true },
+        { i: 'c', x: 1, y: 3, w: 1, h: 2, static: true },
+        { i: 'g', x: 0, y: 5, w: 1, h: 1, static: true },
+        { i: 'h', x: 1, y: 5, w: 1, h: 1, static: true },
+        { i: 'i', x: 0, y: 6, w: 1, h: 1, static: true },
+        { i: 'j', x: 1, y: 6, w: 1, h: 1, static: true },
+        { i: 'k', x: 0, y: 7, w: 2, h: 1, static: true },
     ]
 
     const [rowHeight, setRowHeight] = useState(280)
--- components/bento/IconBox.tsx
+++ components/bento/IconBox.tsx
@@ -6,8 +6,8 @@ import Image from 'next/image'
 const IconBox = ({ icon: Icon, href }) => {
     return (
         <div className="relative flex h-full w-full items-center justify-center rounded-lg">
+            {Icon && <Icon size={96} className="text-primary" />}
             <Link href={href} className="block">
-                {Icon && <Icon size={96} className="text-primary" />}
                 <div className="absolute bottom-0 right-0 m-3 flex w-fit items-end rounded-full border border-border bg-tertiary/50 p-3 text-primary transition-all duration-300 hover:brightness-125">
                     <MoveUpRight size={16} />
                 </div>
