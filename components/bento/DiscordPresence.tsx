1868d8b522f7c9d1ac516b8bd60022aa27229baf
jason <123jasont@gmail.com>
2023-09-20 23:05:48 -0700

    feat(bento): start `DiscordPresence` layout

--- components/bento/DiscordPresence.tsx
+++ components/bento/DiscordPresence.tsx
@@ -1,3 +1,5 @@
+import { FaDiscord } from "react-icons/fa"
+
 const DiscordPresence = ({ lanyard }) => {
     const mainActivity = lanyard.data.activities.filter(
         (activity) =>
@@ -6,13 +8,20 @@ const DiscordPresence = ({ lanyard }) => {
     console.log(mainActivity)
 
     return (
-        <div className="flex flex-col">
+        <div className="relative w-full h-full flex flex-col">
             <img
-                className="w-8 h-8 rounded-full"
+                className="absolute top-9 left-4 w-24 h-24 rounded-full"
                 src={`https://api.lanyard.rest/${lanyard.data.discord_user.id}.png`}
                 alt="Discord Avatar"
             />
-            <div className="text-sm font-medium text-white">
+            <div className="absolute right-0 top-0 z-[1] w-16 h-16 flex items-center justify-center m-3 rounded-full bg-primary">
+                <FaDiscord size={58} className="text-secondary p-1" />
+            </div>
+            <div className="bg-tertiary/50 w-full h-24 rounded-t-3xl" />
+            <div className="my-3 flex w-full h-6">
+                <div className="bg-tertiary/50 rounded-md w-[40%] h-full mr-3 ml-auto" />
+            </div>
+            {/* <div className="text-sm font-medium text-white">
                 @{lanyard.data.discord_user.username}
             </div>
             <div className="text-xs font-medium text-gray-300">
@@ -36,7 +45,7 @@ const DiscordPresence = ({ lanyard }) => {
                 className="w-8 h-8 rounded-full"
                 src={`https://cdn.discordapp.com/app-assets/${mainActivity.application_id}/${mainActivity.assets.small_image}.png`}
                 alt="Discord Avatar"
-            />
+            /> */}
         </div>
     )
 }
