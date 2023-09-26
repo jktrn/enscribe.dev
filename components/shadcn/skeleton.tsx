52d450d06b5d37410ae70313a8516ee422a7bc9f
Jason <123jasont@gmail.com>
2023-09-26 16:52:34 -0700

    feat: custom skeleton animation

--- components/shadcn/skeleton.tsx
+++ components/shadcn/skeleton.tsx
@@ -1,7 +1,7 @@
 import { cn } from '@/scripts/utils/tailwind-helpers'
 
 function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-    return <div className={cn('animate-pulse rounded-md bg-muted', className)} {...props} />
+    return <div className={cn('animate-skeleton rounded-md bg-muted', className)} {...props} />
 }
 
 export { Skeleton }
--- tailwind.config.js
+++ tailwind.config.js
@@ -99,6 +99,7 @@ module.exports = {
             animation: {
                 'accordion-down': 'accordion-down 0.2s ease-out',
                 'accordion-up': 'accordion-up 0.2s ease-out',
+                'skeleton': 'skeleton 2s infinite',
             },
             typography: ({ theme }) => ({
                 DEFAULT: {
