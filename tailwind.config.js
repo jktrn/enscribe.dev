537c8706fb55fe695172b37292b388cdd15eb437
Jason <123jasont@gmail.com>
2023-09-26 17:04:57 -0700

    fix(bento): bad custom skeleton animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -99,7 +99,7 @@ module.exports = {
             animation: {
                 'accordion-down': 'accordion-down 0.2s ease-out',
                 'accordion-up': 'accordion-up 0.2s ease-out',
-                'skeleton': 'skeleton 2s infinite',
+                'skeleton': 'skeleton 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
             },
             typography: ({ theme }) => ({
                 DEFAULT: {
