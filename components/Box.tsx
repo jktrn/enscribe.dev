68c0b4c8f754a86abfa211abe81fa6e7e07dd52a
Jason <123jasont@gmail.com>
2023-10-01 11:47:41 -0700

    chore(blog): import matchmaker writeup

--- components/Box.tsx
+++ components/Box.tsx
@@ -2,9 +2,10 @@ import { AlertCircle, Book, CheckCircle2, Flag, Info } from 'lucide-react'
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import rehypeKatex from 'rehype-katex'
+import rehypeRaw from 'rehype-raw'
 import remarkMath from 'remark-math'
 
-const Box = ({ text, type }) => {
+const Box = ({ text, type = 'default' }) => {
     const typeMapping = {
         warning: {
             color: 'bg-destructive/30',
@@ -26,17 +27,24 @@ const Box = ({ text, type }) => {
             color: 'bg-primary/25',
             icon: Flag,
         },
+        default: {
+            color: 'bg-secondary',
+        },
     }
 
     return (
         <div className={`my-6 rounded-lg p-4 ${typeMapping[type].color} text-center`}>
-            <span className="mb-1 mr-2 inline-flex items-center justify-center align-middle">
-                {React.createElement(typeMapping[type].icon, { size: 16 })}
-            </span>
+            {typeMapping[type].icon && (
+                <span className="mb-1 mr-2 inline-flex items-center justify-center align-middle">
+                    {React.createElement(typeMapping[type].icon, { size: 16 })}
+                </span>
+            )}
             <ReactMarkdown
                 components={{ p: React.Fragment }}
                 remarkPlugins={[remarkMath]}
-                rehypePlugins={[rehypeKatex]}
+                // @ts-ignore
+                rehypePlugins={[rehypeKatex, rehypeRaw]}
+                skipHtml={false}
             >
                 {text}
             </ReactMarkdown>
