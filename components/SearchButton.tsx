0a289c17f2260c1747cbd03c892f994657e57f82
Jason <123jasont@gmail.com>
2023-10-01 14:59:33 -0700

    feat: new `<kbd>` hint in header

--- components/SearchButton.tsx
+++ components/SearchButton.tsx
@@ -1,5 +1,5 @@
 import siteMetadata from '@/data/siteMetadata'
-import { Search } from 'lucide-react'
+import { Command, Search } from 'lucide-react'
 import { AlgoliaButton } from 'pliny/search/AlgoliaButton'
 import { KBarButton } from 'pliny/search/KBarButton'
 
@@ -12,9 +12,17 @@ const SearchButton = () => {
             siteMetadata.search.provider === 'algolia' ? AlgoliaButton : KBarButton
 
         return (
-            <SearchButtonWrapper aria-label="Search">
-                <Search />
-            </SearchButtonWrapper>
+            <>
+                <SearchButtonWrapper aria-label="Search">
+                    <Search />
+                </SearchButtonWrapper>
+                <span className="hidden md:flex gap-1 text-xs text-muted-foreground">
+                    <kbd className="flex items-center justify-center !w-6 !h-6 border border-border rounded-md">
+                        <Command size={12} />
+                    </kbd>
+                    <kbd className="flex items-center justify-center !w-6 !h-6 p-1 border border-border rounded-md">K</kbd>
+                </span>
+            </>
         )
     }
 }
