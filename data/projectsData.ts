c2fc7b6c2e2aa9708c0006ddb4bf261f52987c00
Jason <123jasont@gmail.com>
2023-10-01 13:14:20 -0700

    chore: update metadata

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -4,7 +4,7 @@ const projectsData = [
         description:
             'Built with Next.js, TypeScript, Tailwind CSS, and deployed through Vercel, this bleeding-edge website is both an information security blog and a personal website for my cybersecurity shenanigans and web development ramblings.',
         imgSrc: '/static/images/twitter-card.png',
-        href: 'https://test-blog-v2-jktrn.vercel.app/',
+        href: 'https://github.com/jktrn/enscribe.dev/',
     },
 ]
 
