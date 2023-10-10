fb3de712c171d4b5cccde2a7f31dc1c688447169
Jason <123jasont@gmail.com>
2023-10-09 23:10:16 -0700

    feat: add thumbnails to `ListLayout`

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -12,6 +12,9 @@ export default function Projects() {
                     <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-foreground sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
                         Projects
                     </h1>
+                    <p className="text-muted-foreground">
+                        Stuff I've personally developed or contributed to!
+                    </p>
                 </div>
                 <div className="py-12">
                     <div className="-m-4 flex flex-wrap">
