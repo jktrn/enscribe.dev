eda3c368ae25bf053b7a2842f78c3b51b2773e34
jason <123jasont@gmail.com>
2024-05-13 19:16:37 -0700

    revert: turns out hardcoding templates is bad

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 import { allBlogs } from 'contentlayer/generated'
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
-import { genPageMetadata } from './seo'
 
 import Main from './main'
-
-export const metadata = genPageMetadata({ title: 'Home | enscribe.dev' })
 
 export default async function Page() {
     const sortedPosts = sortPosts(allBlogs)
