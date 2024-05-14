59ee4a7d6bc134d76c2522f68201c42f202df97a
jason <123jasont@gmail.com>
2024-05-13 19:00:52 -0700

    chore: prepend "Home |" to homepage title

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import { allBlogs } from 'contentlayer/generated'
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
+import { genPageMetadata } from './seo'
 
 import Main from './main'
+
+export const metadata = genPageMetadata({ title: 'Home | enscribe.dev' })
 
 export default async function Page() {
     const sortedPosts = sortPosts(allBlogs)
