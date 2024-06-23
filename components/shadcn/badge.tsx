f4d75928311fc62b5489e21bad22634a1d78c570
Jason <123jasont@gmail.com>
2024-06-23 12:14:51 -0700

    chore: cleanup, add project

--- components/shadcn/badge.tsx
+++ components/shadcn/badge.tsx
@@ -13,7 +13,7 @@ const badgeVariants = cva(
                     'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
                 destructive:
                     'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
-                outline: 'text-foreground',
+                outline: 'border-border text-foreground',
             },
         },
         defaultVariants: {
--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const { withContentlayer } = require('next-contentlayer')
-const redirects = require('./data/redirects.js')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 })
